d9a85aaf9c6568711cc9eefac602f02b8446e23b
Alexis Plantin <alexis.plantin@gmail.com>
2022-03-24 16:15:35 +0100

    Add A record on private DNS zone

--- templates/private-dns-zone.tf
+++ templates/private-dns-zone.tf
@@ -9,12 +9,12 @@ resource "azurerm_private_dns_zone" "contoso_internal" {
 }
 
 
-resource "azurerm_private_dns_a_record" "johndoe_contoso_internal" {
-  name                = "johndoe"
+resource "azurerm_private_dns_a_record" "onpremise_vm_contoso_internal" {
+  name                = "onpremise-vm"
   zone_name           = azurerm_private_dns_zone.contoso_internal.name
   resource_group_name = azurerm_resource_group.onpremise-rg.name
   ttl                 = 300
-  records             = ["1.2.3.4"]
+  records             = ["10.233.1.4"]
 }
 
 resource "azurerm_private_dns_zone_virtual_network_link" "contoso_onpremise" {
@@ -32,6 +32,22 @@ resource "azurerm_private_dns_zone_virtual_network_link" "contoso_onpremise" {
 resource "azurerm_private_dns_zone" "contoso_azure" {
   name                = "contoso.azure"
   resource_group_name = azurerm_resource_group.hub-rg.name
+}
+
+resource "azurerm_private_dns_a_record" "hub_vm_contoso_internal" {
+  name                = "hub-vm"
+  zone_name           = azurerm_private_dns_zone.contoso_internal.name
+  resource_group_name = azurerm_resource_group.onpremise-rg.name
+  ttl                 = 300
+  records             = ["10.221.1.4"]
+}
+
+resource "azurerm_private_dns_a_record" "spoke01_vm_contoso_internal" {
+  name                = "spoke01-vm"
+  zone_name           = azurerm_private_dns_zone.contoso_internal.name
+  resource_group_name = azurerm_resource_group.onpremise-rg.name
+  ttl                 = 300
+  records             = ["10.221.8.4"]
 }
 
 resource "azurerm_private_dns_zone_virtual_network_link" "contoso_azure" {
